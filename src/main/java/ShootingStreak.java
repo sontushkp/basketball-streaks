1e5649eee0c03d80820458c533f3d046b475a66e
sontushkp <sontushkpaul@gmail.com>
2021-01-19 02:49:56 -0400

    Test commit 3

--- src/main/java/ShootingStreak.java
+++ src/main/java/ShootingStreak.java
@@ -1,5 +1,5 @@
 public class ShootingStreak {
     public static void main(String[] args) {
-        System.out.println("This is the test commit 2");
+        System.out.println("This is the test commit 3");
     }
 }
